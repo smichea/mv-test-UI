4d0740cd30400f3d3e602db42f53a9efaa245d90
smichea <smichea@gmail.com>
2022-10-28 17:02:36 +0200

    change meveo base url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const MEVEO = { REPOSITORY: "default" };
 export const MODULE = "mv-test";
-export const BASE_URL = "/meveo";
+export const BASE_URL = "http://localhost:8080/meveo";
 export const SCHEMA_PATH = `${BASE_URL}/api/rest/entityCustomization/entity/schema`;
 export const PERSISTENCE_PATH = `${BASE_URL}/api/rest/default/persistence`;
 export const CUSTOM_ACTION_PATH = `${BASE_URL}/rest/runCustomAction`;
